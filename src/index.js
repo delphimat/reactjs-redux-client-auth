68fbad665832a83cd226d989e39aa183a1b84c46
Mathieu Delphin <delphi.mat@gmail.com>
2020-09-20 13:15:10 +0200

    create the redux files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
+
+import reducers from './reducers';
+
+import App from './components/App';
+import Welcome from "./components/Welcome";
+import Signup from "./components/auth/Signup";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <Provider store={createStore(reducers, {})}>
+        <BrowserRouter>
+            <App >
+                <Route path="/" exact component={Welcome}></Route>
+                <Route path="/signup"  component={Signup}></Route>
+            </App>
+        </BrowserRouter>
+    </Provider>,
+    document.querySelector('#root')
+)
