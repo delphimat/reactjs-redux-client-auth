d37f69ff904f5864ed45d3cf9dea710ac625e24b
Mathieu Delphin <delphi.mat@gmail.com>
2020-09-20 21:26:10 +0200

    all the files to auth with the backend made with express.js and mango db

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Header from "./Header";
+import React from 'react';
+import Header from './Header';
 
 export default ({ children }) => {
-    return (
-        <div>
-            <Header />
-            {children}
-        </div>
-    );
-}
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
-
 import App from './components/App';
-import Welcome from "./components/Welcome";
-import Signup from "./components/auth/Signup";
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
+import Feature from './components/Feature';
+import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
+
+const store = createStore(
+  reducers,
+  {
+    auth: { authenticated: localStorage.getItem('token') }
+  },
+  applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
-    <Provider store={createStore(reducers, {})}>
-        <BrowserRouter>
-            <App >
-                <Route path="/" exact component={Welcome}></Route>
-                <Route path="/signup"  component={Signup}></Route>
-            </App>
-        </BrowserRouter>
-    </Provider>,
-    document.querySelector('#root')
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/feature" component={Feature} />
+        <Route path="/signout" component={Signout} />
+        <Route path="/signin" component={Signin} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('#root')
+);
